cc4519d86435a5f3561aaf02dc38db340194cf84
Native-Speaker <info@nativespeaker.ie>
2026-09-03 22:03:58 +0100

    Update onboard.js

--- api/onboard.js
+++ api/onboard.js
@@ -54,11 +54,10 @@ export default async function handler(req, res) {
     first_name: firstName,
     last_name: lastName,
     password: password,
-    role: 'learner',
   });
 
   try {
-    const response = await fetch(`https://${TALENTLMS_DOMAIN}/api/v1/users`, {
+    const response = await fetch(`https://${TALENTLMS_DOMAIN}/api/v1/usersignup`, {
       method: 'POST',
       headers: {
         Authorization: authHeader,
